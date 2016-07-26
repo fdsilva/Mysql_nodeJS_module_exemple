b2290a6bc289ed6d0b3a41d633ea23da3eb8b44c
fdsilva <neitonfsilva@gmail.com>
2016-07-25 22:34:49 -0700

    Finishing CRUD examples

---
+++ index.js
@@ -0,0 +1,60 @@
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "sitepoint"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+var employee = { name: 'Winnie', location: 'Australia' };
+con.query('INSERT INTO employees SET ?', employee, function(err,res){
+  if(err) throw err;
+
+  console.log('Last insert ID:', res.insertId);
+});
+
+con.query(
+  'UPDATE employees SET location = ? Where ID = ?',
+  ["South Africa", 5],
+  function (err, result) {
+    if (err) throw err;
+
+    console.log('Changed ' + result.changedRows + ' rows');
+  }
+);
+
+con.query(
+  'DELETE FROM employees WHERE id = ?',
+  [5],
+  function (err, result) {
+    if (err) throw err;
+
+    console.log('Deleted ' + result.affectedRows + ' rows');
+  }
+);
+
+con.query('SELECT * FROM employees',function(err,rows){
+  if(err) throw err;
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+});
+
+
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
